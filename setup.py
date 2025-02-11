4fe859a6bdfc44839e64f096deacfc0a63a7e9c9
gauravcodebytes <167018967+gauravcodebytes@users.noreply.github.com>
2025-02-12 01:28:25 +0530

    mongodb demo added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Gaurav",
+    author_email="gauravchauhan2019@gmail.com",
+    packages=find_packages()
+)
